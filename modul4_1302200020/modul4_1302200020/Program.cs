d3931cbc1dbf69949dbbeea6615952dfdb254b68
Heiasan <abdillahhasan130@gmail.com>
2022-03-19 17:26:02 +0700

    menambahkan class Penjumlahan dengan methodJumlahTigaAngka

---
+++ modul4_1302200020/modul4_1302200020/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace modul4_1302200020
+{
+     public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // NIM = 1302200020
+            Penjumlahan.JumlahTigaAngka<long>(13, 02, 20);
+        }
+    }
+}
